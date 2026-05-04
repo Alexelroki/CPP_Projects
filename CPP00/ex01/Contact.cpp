bee021d5c6d0f219a0c1c8e7735d572196cbfb28
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-04 11:45:35 +0200

    feat: implement Contact class with setters and getters

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -1,1 +1,53 @@
 #include "Contact.hpp"
+
+Contact::Contact(): _firstName(""), _lastName(""), _nickName(""), _phoneNumber(""), _darkestSecret("") {}
+Contact::~Contact() {}
+
+void	Contact::setFirstName(const std::string& f)
+{
+	_firstName = f;
+}
+void	Contact::setLastName(const std::string& l)
+{
+	_lastName = l;
+}
+
+void	Contact::setNickName(const std::string& n)
+{
+	_nickName = n;
+}
+
+void	Contact::setPhoneNumber(const std::string& p)
+{
+	_phoneNumber = p;
+}
+
+void	Contact::setDarkestSecret(const std::string& d)
+{
+	_darkestSecret = d;
+}
+
+std::string	Contact::getFirstName() const
+{
+	return (_firstName);
+}
+
+std::string	Contact::getLastName() const
+{
+	return (_lastName);
+}
+
+std::string	Contact::getNickName() const
+{
+	return (_nickName);
+}
+
+std::string	Contact::getPhoneNumber() const
+{
+	return (_phoneNumber);
+}
+
+std::string	Contact::getDarkestSecret() const
+{
+	return (_darkestSecret);
+}
