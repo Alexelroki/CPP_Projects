d8d05315a5b5fed66143cf1e35af255be194d548
Alex <alexelroki@gmail.com>
2026-05-26 22:13:13 +0200

    wip(ex02): keep Account.cpp rebuild in progress
    Capture the current Account.cpp as an intentional work-in-progress baseline. The exercise is still being rebuilt, so this commit records the pending state without presenting it as finished.

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,5 +1,125 @@
 #include "Account.hpp"
 #include <iostream>
+
+// Inicializo variables estáticas
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+// Getters
+int	Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void )
+{
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__________________________________________________________
+#include "Account.hpp"
+#include <iostream>
 #include <ctime>
 #include <iomanip>
 
