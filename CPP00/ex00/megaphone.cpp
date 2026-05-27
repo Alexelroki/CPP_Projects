34573642ffb0c15772c2ed257909fc8260c37722
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-27 14:04:34 +0200

    fix: remove duplicate and commented code from megaphone.cpp
    - Remove duplicate main function
    - Remove unused toUpper function
    - Fix compilation errors with -Werror flag

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -19,26 +19,3 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
-
---------- ///* Otra versión, sin el bucle anidado, pero con un comportamiento diferente (no convierte a mayúscula, sino que imprime tal cual) POR COMPLETAR*/
-std::string toUpper(std::string str)
-{
-	std::string result;
-	for (size_t i = 0; i < str.length(); i++)
-		result += std::toupper(str[i]);
-	return result;
-}
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			std::cout << std::toupper(argv[i]) << " ";
-		}
-		std::cout << std::endl;
-	}
-	return (0);
-}
