941a9ae113d274ca1c1db7788381aa60aebabbdc
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-27 10:52:35 +0200

    fix: increase buffer size in timestamp to prevent overflow

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,5 +1,6 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
 
 // Inicializo variables estáticas
 int	Account::_nbAccounts = 0;
@@ -28,96 +29,121 @@ int	Account::getNbWithdrawals( void )
 	return _totalNbWithdrawals;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-__________________________________________________________
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout
+	<< "accounts:" << _nbAccounts
+	<< ";total" << _totalAmount
+	<< ";deposits:" << _totalNbDeposits
+	<< ";withdrawals:" << _totalNbWithdrawals
+	<< std::endl;
+}
+
+Account::Account( int initial_deposit )
+{
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";created"
+	<< std::endl;
+}
+
+Account::~Account( void )
+{
+	_nbAccounts--;
+	_totalAmount -= this->_amount;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";closed"
+	<< std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	int	p_amount = this->_amount;
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";p_amount:" << p_amount
+	<< ";deposit:" << deposit
+	<< ";amount:" << this->_amount
+	<< ";nb_deposits:" << this->_nbDeposits
+	<< std::endl;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";p_amount:" << this->_amount;
+
+	if (this->_amount < withdrawal)
+	{
+		std::cout << ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+
+	std::cout
+	<< ";withdrawal:" << withdrawal
+	<< ";amount:" << this->_amount
+	<< ";_nbWithdrawals:" << this->_nbWithdrawals
+	<< std::endl;
+
+	return (true);
+}
+
+int	Account::checkAmount( void ) const
+{
+	return (this->_amount);
+}
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";deposits:" << this->_nbDeposits
+	<< ";withdrawals:" << this->_nbWithdrawals
+	<< std::endl;
+}
+
+void	Account::_displayTimestamp( void )
+{
+	time_t		rawtime;
+	struct tm	*timeinfo;
+	char		buffer[20];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", timeinfo);
+	std::cout << buffer;
+}
+/*_________________________________________________________
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
@@ -198,3 +224,4 @@ void Account::displayStatus(void) const {
     std::cout << "index:" << _accountIndex << ";amount:" << _amount 
               << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
+*/
