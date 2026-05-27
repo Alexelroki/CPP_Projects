117a7c5af36b822537c9efe5304b83018700b6da
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-27 13:57:41 +0200

    refactor: reorganize Account.cpp and fix constructor syntax error
    - Move constructor and destructor to the beginning after static variables
    - Remove syntax error (semicolon before curly brace in constructor)
    - Delete generated log file

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -2,13 +2,41 @@
 #include <iostream>
 #include <ctime>
 
-// Inicializo variables estáticas
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
-// Getters
+Account::Account( int initial_deposit ):
+	_accountIndex(_nbAccounts),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
+{
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";created"
+	<< std::endl;
+}
+
+Account::~Account( void )
+{
+	_nbAccounts--;
+	_totalAmount -= this->_amount;
+
+	_displayTimestamp();
+	std::cout
+	<< "index:" << this->_accountIndex
+	<< ";amount:" << this->_amount
+	<< ";closed"
+	<< std::endl;
+}
+
 int	Account::getNbAccounts( void )
 {
 	return _nbAccounts;
@@ -37,36 +65,6 @@ void	Account::displayAccountsInfos( void )
 	<< ";total:" << _totalAmount
 	<< ";deposits:" << _totalNbDeposits
 	<< ";withdrawals:" << _totalNbWithdrawals
-	<< std::endl;
-}
-
-Account::Account( int initial_deposit ):
-	_accountIndex(_nbAccounts),
-	_amount(initial_deposit),
-	_nbDeposits(0),
-	_nbWithdrawals(0);
-{
-	_nbAccounts++;
-	_totalAmount += initial_deposit;
-
-	_displayTimestamp();
-	std::cout
-	<< "index:" << this->_accountIndex
-	<< ";amount:" << this->_amount
-	<< ";created"
-	<< std::endl;
-}
-
-Account::~Account( void )
-{
-	_nbAccounts--;
-	_totalAmount -= this->_amount;
-
-	_displayTimestamp();
-	std::cout
-	<< "index:" << this->_accountIndex
-	<< ";amount:" << this->_amount
-	<< ";closed"
 	<< std::endl;
 }
 
