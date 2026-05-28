958dc3b2eac23ba6d2ced99b767bfe170b82a88e
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-28 15:59:36 +0200

    Fix: atribute name and include string library

--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -1,16 +1,17 @@
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
-<string>
+# include <string>
 
 class Zombie
 {
 	private:
-		std::string	name;
+		std::string	_name;
 	public:
 		Zombie( std::string name );
 		~Zombie();
-		void	announce( void );
+
+		void		announce( void );
 };
 
 Zombie*	newZombie( std::string name );
