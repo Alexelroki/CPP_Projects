d6f952edc357a9fc7ce03d2e74d99ccb8f12a464
alexelroki/42campusMalaga <alexelroki@gmail.com>
2026-05-30 11:46:04 +0200

    feat: implement Harl class with dispatcher pattern using function pointers
    - Implement four private complaint methods with distinct messages
    - Implement complain() with array-based dispatcher pattern
    - Use pointer-to-member syntax for method invocation
    - Handle unknown complaint levels with default message
    - Demonstrates C++98 function pointer array technique

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,48 @@
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl( void ) {}
+Harl::~Harl( void ) {}
+
+void	Harl::_debug( void ) const
+{
+	std::cout << "[ DEBUG ]\nI love having extra bacon for my burger. I really do!" << std::endl;
+}
+
+void	Harl::_info( void ) const
+{
+	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money." << std::endl;
+}
+
+void	Harl::_warning( void ) const
+{
+	std::cout << "[ WARNING ]\nI think I deserve some free extra bacon." << std::endl;
+}
+
+void	Harl::_error( void ) const
+{
+	std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain( const std::string& level ) const
+{
+	std::string	levels[4] = { "DEBUG", "INFO", "WARNING", "ERROR", };
+
+	void	(Harl::*methods[4])( void ) const =
+	{
+		&Harl::_debug,
+		&Harl::_info,
+		&Harl::_warning,
+		&Harl::_error
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*methods[i])();
+			return ;
+		}
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+}
