efad4e6e72b74f3a9d278ec9b3efaedc59a774fe
Alex <alexelroki@gmail.com>
2026-06-04 22:01:41 +0200

    feat(claptrap): define ClapTrap class interface in Orthodox Canonical Form

---
+++ CPP03/ex02/ClapTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+
+class ClapTrap
+{
+	protected:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+
+	public:
+		// Orthodox Canonical Form
+		ClapTrap( void );
+		ClapTrap( const std::string& name );
+		ClapTrap( const ClapTrap& other );
+		ClapTrap& operator=( const ClapTrap& other );
+		~ClapTrap( void );
+
+		// Fight functions
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
+
+#endif
