3d5aa43e763ae2e134b59e7059bc08fadbae9911
Alex <alexelroki@gmail.com>
2026-06-04 22:02:24 +0200

    feat(scavtrap): define ScavTrap derived class interface with guardGate capability

---
+++ CPP03/ex02/ScavTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+// ScavTrap publicly inherits from ClapTrap
+class ScavTrap : public ClapTrap
+{
+	public:
+		// Orthodox Canonical Form
+		ScavTrap( void );
+		ScavTrap( const std::string& name );
+		ScavTrap( const ScavTrap& other );
+		ScavTrap& operator=( const ScavTrap& other );
+		~ScavTrap( void );
+
+		// We redefined attack because the statement says it has its own message
+		void    attack( const std::string& target );
+		
+		// ScavTrap's unique special capability
+		void    guardGate( void );
+};
+
+#endif
