9b3a690094a0eae01a0f1bb4f9e53e58c739e69f
Alex <alexelroki@gmail.com>
2026-06-04 22:03:12 +0200

    feat(fragtrap): define FragTrap derived class interface with highFivesGuys capability

---
+++ CPP03/ex02/FragTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	public:
+		// Orthodox Canonical Form
+		FragTrap( void );
+		FragTrap( const std::string& name );
+		FragTrap( const FragTrap& other );
+		FragTrap& operator=( const FragTrap& other );
+		~FragTrap( void );
+
+		// Unique special capability
+		void    highFivesGuys( void );
+};
+
+#endif
