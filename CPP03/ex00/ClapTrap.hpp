e0ef52dc780a8278f864355a5f5123c05c8fba74
alexelroki <alexelroki@gmail.com>
2026-06-04 14:25:34 +0200

    feat: add ClapTrap header file with Orthodox Canonical Form
    - Define ClapTrap class with private attributes (name, hitPoints, energyPoints, attackDamage)
    - Implement Orthodox Canonical Form (default constructor, copy constructor, assignment operator, destructor)
    - Add public methods: attack, takeDamage, beRepaired

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -11,15 +11,15 @@ class ClapTrap
 		unsigned int	_energyPoints;
 		unsigned int	_attackDamage;
 
-		ClapTrap( void );
-
 	public:
 		// Orthodox Canonical Form
-		ClapTrap( const std::string name );
+		ClapTrap( void );
+		ClapTrap( const std::string& name );
 		ClapTrap( const ClapTrap& other );
 		ClapTrap& operator=( const ClapTrap& other );
 		~ClapTrap( void );
 
+		// Fight functions
 		void	attack(const std::string& target);
 		void	takeDamage(unsigned int amount);
 		void	beRepaired(unsigned int amount);
