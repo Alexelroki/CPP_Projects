2facd0f4761b91a1a3986cf4f20b6b7211fc7a0b
alexelroki <alexelroki@gmail.com>
2026-06-06 18:17:14 +0200

    feat: create constructors and assignment operator

--- CPP03/ex03/DiamondTrap.cpp
+++ CPP03/ex03/DiamondTrap.cpp
@@ -1,16 +1,49 @@
 DiamondTrap::DiamondTrap( void )
-	: ClapTrap("Default_clap_name"), ScavTrap(), FragTrap()
+	: ClapTrap("Default_clap_name"), ScavTrap(), FragTrap(), _name("Default")
 {
-	this->name = "Default";
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+	std::cout << "DiamondTrap default constructor called" << std::endl;
 }
 
-DiamondTrap::DiamondTrap( const std::string& name );
-DiamondTrap::DiamondTrap( const DiamondTrap& other );
-DiamondTrap&	DiamondTrap::operator=( const DiamondTrap& other );
-DiamondTrap::~DiamondTrap( void );
+DiamondTrap::DiamondTrap( const std::string& name )
+	: ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), _name(name) 
+{
+	this->_hitPoints = FragTrap::_hitPoints;
+	this->_energyPoints = ScavTrap::_energyPoints;
+	this->_attackDamage = FragTrap::_attackDamage;
+	std::cout << "DiamondTrap name constructor called" << std::endl;
+}
+
+DiamondTrap::DiamondTrap( const DiamondTrap& other )
+	: ClapTrap(other), ScavTrap(other), FragTrap(other), _name(other._name)
+{
+	*this = other;
+	std::cout << "DiamondTrap copy constructor called" << std::endl;
+}
+
+DiamondTrap&	DiamondTrap::operator=( const DiamondTrap& other )
+{
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+		this->_name = other._name;
+	}
+	std::cout << "DiamondTrap copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+DiamondTrap::~DiamondTrap( void )
+{
+	std::cout << "DiamondTrap destructor called" << std::endl;
+}
 
 // We need to choose the FragTrap implementation
-void	DiamondTrap::attack( const std::string& target );
+void	DiamondTrap::attack( const std::string& target )
+{
+	ScavTrap::attack(target);
+}
 
 // DiamondTrap special capability
 void	DiamondTrap::whoAmI( void );
