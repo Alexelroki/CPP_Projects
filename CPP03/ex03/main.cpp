969a3016d2159d295f54b15b342f1eab1c6ec950
alexelroki <alexelroki@gmail.com>
2026-06-07 19:23:09 +0200

    feat: add tests for every situation

--- CPP03/ex03/main.cpp
+++ CPP03/ex03/main.cpp
@@ -1,18 +1,47 @@
-#include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 #include <iostream>
+
+// ClapTrap's name -> [name + "_clap_name"], ScavTrap and FragTraps inherit the ClapTrap's name
+// Hit Points -> FragTrap -> 100
+// Energy Points -> ScavTrap -> 50
+// Attack Damage -> FragTrap -> 30
+// attack() -> ScavTrap
 
 int main( void )
 {
-	std::cout << "--- CREATING FRAGTRAP ---" << std::endl;
-	FragTrap frag("FR4G-TP");
+	std::cout << "--- CREATING DIAMONDTRAP ---" << std::endl;
+	DiamondTrap	diamond("DI4M-TP");
+	DiamondTrap	copy1("C0PY-TP");
 
 	std::cout << "\n--- ACTION AND ATTACK TEST ---" << std::endl;
-	frag.attack("a dangerous bandit"); // Uses ClapTrap's attack
-	frag.takeDamage(40);
-	frag.beRepaired(20);
+	diamond.attack("a dangerous bandit"); // Uses ScavTrap's attack
+	diamond.guardGate(); // Uses ScavTrap's function
+	diamond.takeDamage(40); // Uses ClapTrap's function
+	diamond.beRepaired(20); // Uses ClapTrap's function
+	diamond.whoAmI();
+	diamond.highFivesGuys();
 
-	std::cout << "\n--- SPECIAL ABILITY TEST ---" << std::endl;
-	frag.highFivesGuys();
+	std::cout << "\n--- ASSIGNMENT AND COPY TEST ---" << std::endl;
+
+	std::cout << "1. \n";
+	copy1.whoAmI();
+	copy1 = diamond;
+	copy1.takeDamage(1);
+	copy1.whoAmI();
+
+	std::cout << "2. \n";
+	DiamondTrap	copy2(diamond);
+	copy2.takeDamage(1);
+	copy2.whoAmI();
+
+	/*std::cout << "\n--- ENERGY TEST ---" << std::endl;
+	DiamondTrap	energytest("EN3RGY-TP");
+	for (int i = 1; i <= 100; i++)
+	{
+		std::cout << i << ". ";
+		energytest.attack("a dangerous bandit"); // 100 energy points consumed
+	}
+	energytest.attack("a dangerous bandit"); // No energy to attack*/
 
 	std::cout << "\n--- DESTRUCTION CHAIN ---" << std::endl;
 	return (0);
