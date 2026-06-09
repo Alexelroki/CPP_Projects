1d67616db412c1afcb63d2f0f93db0e15d8392e7
alexelroki <alexelroki@gmail.com>
2026-06-09 15:02:40 +0200

    test: rewrite main.cpp to expand test cases
        - Add specific scenarios and edge cases to thorougly test class
          behavior.

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -3,17 +3,37 @@
 
 int main( void )
 {
-	std::cout << "--- SCAVTRAP CREATION ---" << std::endl;
-	ScavTrap scav("SC4V-TP");
-
-	std::cout << "\n--- ACTION AND ATTACK TEST ---" << std::endl;
-	scav.attack("an aggressive psycho");
-	scav.takeDamage(30);
-	scav.beRepaired(15);
-
-	std::cout << "\n--- SPECIAL ABILITY TEST ---" << std::endl;
+	std::cout << "--- ROBOT CREATION ---" << std::endl;
+	ScavTrap	scav("SC4V-TP");
+	
+	std::cout << "\n--- ACTION TEST ---" << std::endl;
+	scav.attack("a wild bullymong"); // 1 energy point less (9)
+	scav.takeDamage(5);
+	scav.beRepaired(3); // 1 energy point less (8)
 	scav.guardGate();
 
-	std::cout << "\n--- DESTRUCTION CHAIN ---" << std::endl;
+	std::cout << "\n--- ENERGY TEST ---" << std::endl;
+	// Already spent 2 energy (1 attack + 1 repair). 8 remain.
+	for (int i = 1; i <= 48; i++)
+	{
+		std::cout << i << ". ";
+		scav.attack("an enemy");
+	}
+
+	std::cout << "\n--- THIS SHOULDN'T WORK ---" << std::endl;
+	scav.attack("an enemy");
+	scav.beRepaired(100);
+	scav.guardGate();
+
+	std::cout << "\n--- DEAD TEST ---" << std::endl;
+	ScavTrap	fragile("Porcellaneous");
+	fragile.takeDamage(150); // Receives more damage than the life he has
+
+	std::cout << "\n--- THIS SHOULDN'T WORK ---" << std::endl;
+	fragile.attack("someone"); // It shouldn't be able to do anything
+	fragile.beRepaired(2);
+	fragile.guardGate();
+
+	std::cout << "\n---🚨 AUTODESTRUCTION 🚨 ---" << std::endl;
 	return (0);
 }
