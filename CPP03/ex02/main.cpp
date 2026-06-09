0baa51951ad39cf0ed79f5564e13a30853688bfb
alexelroki <alexelroki@gmail.com>
2026-06-09 15:30:25 +0200

    test: rewrite main.cpp to expand test cases
        - Add specific scenarios and edge cases to thorougly test class
          behavior.

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -1,18 +1,38 @@
 #include "FragTrap.hpp"
 #include <iostream>
 
-int main( void )
+int	main( void )
 {
 	std::cout << "--- CREATING FRAGTRAP ---" << std::endl;
-	FragTrap frag("FR4G-TP");
-
-	std::cout << "\n--- ACTION AND ATTACK TEST ---" << std::endl;
-	frag.attack("a dangerous bandit"); // Uses ClapTrap's attack
-	frag.takeDamage(40);
-	frag.beRepaired(20);
-
-	std::cout << "\n--- SPECIAL ABILITY TEST ---" << std::endl;
+	FragTrap	frag("FR4G-TP");
+	
+	std::cout << "\n--- ACTION TEST ---" << std::endl;
+	frag.attack("a wild bullymong"); // 1 energy point less (9)
+	frag.takeDamage(5);
+	frag.beRepaired(3); // 1 energy point less (8)
 	frag.highFivesGuys();
+
+	std::cout << "\n--- ENERGY TEST ---" << std::endl;
+	// Already spent 2 energy (1 attack + 1 repair). 8 remain.
+	for (int i = 1; i <= 98; i++)
+	{
+		std::cout << i << ". ";
+		frag.attack("an enemy");
+	}
+
+	std::cout << "\n--- THIS SHOULDN'T WORK ---" << std::endl;
+	frag.attack("an enemy");
+	frag.beRepaired(100);
+	frag.highFivesGuys();
+
+	std::cout << "\n--- DEAD TEST ---" << std::endl;
+	FragTrap	fragile("Porcellaneous");
+	fragile.takeDamage(150); // Receives more damage than the life he has
+
+	std::cout << "\n--- THIS SHOULDN'T WORK ---" << std::endl;
+	fragile.attack("someone"); // It shouldn't be able to do anything
+	fragile.beRepaired(2);
+	fragile.highFivesGuys();
 
 	std::cout << "\n--- DESTRUCTION CHAIN ---" << std::endl;
 	return (0);
