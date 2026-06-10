c08e5109c4b92b2c74f0258acf330acf7953747f
alexelroki <alexelroki@gmail.com>
2026-06-10 11:56:08 +0200

    feat: implement Cat derived class implementation

---
+++ CPP04/ex00/Cat.cpp
@@ -0,0 +1,35 @@
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat( void ) : Animal()
+{
+	this->type = "Cat";
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat( const Cat& other ) : Animal(other)
+{
+	this->type = other.type;
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat&	Cat::operator=( const Cat& other )
+{
+	if (this != &other)
+	{
+		Animal::operator=(other);
+		this->type = other.type;
+	}
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+Cat::~Cat( void )
+{
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void	Cat::makeSound( void ) const
+{
+	std::cout << "Meow, meow!" << std::endl;
+}
