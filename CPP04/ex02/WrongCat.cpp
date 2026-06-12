72488b0459c8a824267a9d5f060d11ff8a72d672
alexelroki <alexelroki@gmail.com>
2026-06-12 15:50:27 +0200

    feat: add brain initialization and liberation

--- CPP04/ex02/WrongCat.cpp
+++ CPP04/ex02/WrongCat.cpp
@@ -1,13 +1,13 @@
 #include "WrongCat.hpp"
 #include <iostream>
 
-WrongCat::WrongCat( void ) : WrongAnimal()
+WrongCat::WrongCat( void ) : WrongAnimal(), _brain(new Brain())
 {
 	this->type = "WrongCat";
 	std::cout << "WrongCat default constructor called" << std::endl;
 }
 
-WrongCat::WrongCat( const WrongCat& other ) : WrongAnimal(other)
+WrongCat::WrongCat( const WrongCat& other ) : WrongAnimal(other), _brain(new Brain(*other._brain))
 {
 	this->type = other.type;
 	std::cout << "WrongCat copy constructor called" << std::endl;
@@ -26,6 +26,7 @@ WrongCat&	WrongCat::operator=( const WrongCat& other )
 
 WrongCat::~WrongCat( void )
 {
+	delete _brain;
 	std::cout << "WrongCat destructor called" << std::endl;
 }
 
