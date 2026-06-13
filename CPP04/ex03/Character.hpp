1bae21072841c7c6066f1786334d5dcffac3ed13
alexelroki <alexelroki@gmail.com>
2026-06-13 16:04:50 +0200

    feat: declare Character concrete class

---
+++ CPP04/ex03/Character.hpp
@@ -0,0 +1,28 @@
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include "ICharacter.hpp"
+# include "AMateria.hpp"
+# include <string>
+
+class Character : public ICharacter
+{
+	public:
+		// Orthodox Canonical Form
+		Character( const std::string& name );
+		Character( const Character& other );
+		Character	operator=( const Character& other );
+		virtual ~Character( void );
+
+		// Contract rules
+		const std::string&	getName() const;
+		void				equip(AMateria* m);
+		void				unequip(int idx);
+		void				use(int idx, ICharacter& target);
+
+	private:
+		std::string			_name;
+		AMateria*			_inventory[4];
+}
+
+#endif
