551f65ebac8e5c9f05e07de3edec20b052b2ccc8
alexelroki <alexelroki@gmail.com>
2026-07-17 12:51:47 +0200

    feat(CPP05/ex02)[DONE]: PresidentialPardonForm.hpp definition complete.

---
+++ CPP05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,24 @@
+#ifndef PRESIDENTIALPARDONFORM
+# define PRESIDENTIALPARDONFORM
+
+# include "AForm.hpp"
+# include <cstdlib>
+# include <ctime>
+
+class PresidentialPardonForm : public AForm
+{
+	public:
+		PresidentialPardonForm( const std::string& target );
+		PresidentialPardonForm( const PresidentialPardonForm& other );
+		virtual ~PresidentialPardonForm( void );
+
+	protected:
+		virtual void	executeAction( void ) const;
+
+	private:
+		const std::string	_target;
+		PresidentialPardonForm( void );
+		PresidentialPardonForm&	operator=( const PresidentialPardonForm& other );
+};
+
+#endif
