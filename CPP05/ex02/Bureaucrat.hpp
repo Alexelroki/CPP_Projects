644f63a751105ab29abebbfe37e5ca7575578d15
alexelroki <alexelroki@gmail.com>
2026-07-17 12:51:47 +0200

    feat(CPP05/ex02)[DONE]: Bureaucrat.hpp update complete.

---
+++ CPP05/ex02/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+# include <exception>
+
+class AForm; // --> signForm()
+
+class Bureaucrat
+{
+	public:
+		// Orthodox Canonical Form
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		~Bureaucrat();
+
+		// Getters
+		const std::string&	getName() const;
+		int					getGrade() const;
+
+		// Member Functions
+		void		incrementGrade();
+		void		decrementGrade();
+		void		signForm(AForm& form) const;
+		void		executeForm(const AForm& form) const;
+
+		// Nested custom exception classes
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+
+	private:
+		const std::string	_name;
+		int					_grade;
+
+		// Disabled assignment operator due to const attribute design constraints
+		Bureaucrat&		operator=(const Bureaucrat& src);
+};
+
+std::ostream&	operator<<(std::ostream& o, const Bureaucrat& instance);
+
+#endif
