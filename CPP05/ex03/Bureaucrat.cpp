65db7413d5c66d79ab2993d15a62c48def85173d
alexelroki <alexelroki@gmail.com>
2026-07-17 15:45:42 +0200

    feat(CPP05/ex03)[DONE]: Bureaucrat.cpp inclusion complete.

---
+++ CPP05/ex03/Bureaucrat.cpp
@@ -0,0 +1,89 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp" // --> signForm()
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150) {}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {}
+
+Bureaucrat::~Bureaucrat() {}
+
+// Getters
+const std::string&	Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+int				Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+// Member Functions
+void			Bureaucrat::incrementGrade()
+{
+	if (this->_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	this->_grade--;
+}
+
+void			Bureaucrat::decrementGrade()
+{
+	if (this->_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade++;
+}
+
+void			Bureaucrat::signForm(AForm& form) const
+{
+	try
+	{
+		form.AForm::beSigned(*this);
+		std::cout << "   " << this->_name << " signed " << form.getName() << ".\n\n";
+	}
+	catch (std::exception& e)
+	{
+		std::cout
+		<< "   " << this->_name << " couldn't sign " << form.getName()
+		<< " because " << e.what() << "\n";
+	}
+}
+
+void			Bureaucrat::executeForm(const AForm& form) const
+{
+	try
+	{
+		form.execute(*this);
+		std::cout << "   " << this->_name << " executed " << form.getName() << ".\n\n";
+	}
+	catch (std::exception& e)
+	{
+		std::cout
+		<< "   " << this->_name << " couldn't execute " << form.getName()
+		<< " because " << e.what() << "\n";
+	}
+}
+
+// Exceptions
+const char*		Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Bureaucrat grade is too high! Maximum allowed is 1.");
+}
+const char*		Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Bureaucrat grade is too low! Minimum allowed is 150.");
+}
+
+// Stream Output Operator
+std::ostream&	operator<<(std::ostream& o, const Bureaucrat& instance)
+{
+	o << instance.getName() << ", bureaucrat grade " << instance.getGrade() << ".";
+	return (o);
+}
