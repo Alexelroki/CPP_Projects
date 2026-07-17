5020a065226c5cb51202ebbe3904e88adc3d1847
alexelroki <alexelroki@gmail.com>
2026-07-17 12:51:47 +0200

    feat(CPP05/ex02)[DONE]: ShrubberyCreationForm.hpp definition complete.

---
+++ CPP05/ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,23 @@
+#ifndef SHRUBBERYCREATIONFORM
+# define SHRUBBERYCREATIONFORM
+
+# include "AForm.hpp"
+# include <fstream>
+
+class ShrubberyCreationForm : public AForm
+{
+	public:
+		ShrubberyCreationForm( const std::string& target );
+		ShrubberyCreationForm( const ShrubberyCreationForm& other );
+		virtual ~ShrubberyCreationForm( void );
+
+	protected:
+		virtual void	executeAction( void ) const;
+
+	private:
+		const std::string	_target;
+		ShrubberyCreationForm( void );
+		ShrubberyCreationForm&	operator=( const ShrubberyCreationForm& other );
+};
+
+#endif
