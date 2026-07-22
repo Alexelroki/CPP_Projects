2b9eee88d77c33f1fa7e57219ac2d89236625398
alexelroki <alexelroki@gmail.com>
2026-07-22 20:21:34 +0200

    fix(CPP06/ex01): delete 'Static' on the method's implementation.

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -15,12 +15,12 @@ Serializer&		Serializer::operator=( const Serializer& other )
 
 Serializer::~Serializer( void ) {}
 
-static uintptr_t	Serializer::serialize( Data* ptr )
+uintptr_t	Serializer::serialize( Data* ptr )
 {
 	return (reinterpret_cast<uintptr_t>(ptr));
 }
 
-static Data*		Serializer::deserialize( uintptr_t raw )
+Data*		Serializer::deserialize( uintptr_t raw )
 {
 	return (reinterpret_cast<Data*>(raw));
 }
