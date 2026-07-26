a8bb2e810e34a0036741eb02751f215c6bd924c1
alexelroki <alexelroki@gmail.com>
2026-07-26 19:31:54 +0200

    fix(CPP06/ex01)[DONE]: separate Serializer interface from Data

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -1,19 +1,13 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
-# include <stdint.h> // uintptr_t - try <cstdint> later
-# include <string>
-
-struct Data // check the Norm V4: s_data, t_data.
-{
-	std::string	s;
-	int		n;
-};
+# include <stdint.h> // uintptr_t - <cstdint> is c++11
+# include "Data.hpp"
 
 class Serializer
 {
 	public:
-		static uintptr_t	serialize( Data* ptr );
+		static uintptr_t		serialize( Data* ptr );
 		static Data*			deserialize( uintptr_t raw );
 	private:
 		// Orthodox Canonical Form
