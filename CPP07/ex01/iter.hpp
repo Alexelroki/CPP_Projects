3b052b45773a4dbc259ab4ccb886bdf06141696c
alexelroki <alexelroki@gmail.com>
2026-07-28 15:02:59 +0200

    style(CPP07/ex01): add space.

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,7 +1,7 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-#include <cstddef> // size_t
+# include <cstddef> // size_t
 
 template <typename T, typename F>
 void		iter(T* array, const size_t length, F func)
